008f04b404a6eb69314b383e51fa7f5dde7b057b
Carson M <carson@pyke.io>
2024-07-01 21:37:25 -0500

    Formatting

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const randomExclude = (min, max, exclude) => {
  */
 const hashCode = str => {
 	let h1 = 0xdeadbeef, h2 = 0x41c6ce57;
-	for(let i = 0, ch; i < str.length; i++) {
+	for (let i = 0, ch; i < str.length; i++) {
 		ch = str.charCodeAt(i);
 		h1 = Math.imul(h1 ^ ch, 2654435761);
 		h2 = Math.imul(h2 ^ ch, 1597334677);
@@ -37,7 +37,6 @@ const hashCode = str => {
 	h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
 	h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
 	h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
-
 	return 4294967296 * (2097151 & h2) + (h1 >>> 0);
 };
 
