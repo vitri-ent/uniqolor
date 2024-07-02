aae82ee8a4106406f7f65ff46463c02579c750a6
Carson M <carson@pyke.io>
2024-07-01 21:36:37 -0500

    Replace string hash function with cyrb53

--- src/index.js
+++ src/index.js
@@ -27,15 +27,18 @@ const randomExclude = (min, max, exclude) => {
  * @return {number}
  */
 const hashCode = str => {
-  const len = str.length;
-  let hash = 0;
+	let h1 = 0xdeadbeef, h2 = 0x41c6ce57;
+	for(let i = 0, ch; i < str.length; i++) {
+		ch = str.charCodeAt(i);
+		h1 = Math.imul(h1 ^ ch, 2654435761);
+		h2 = Math.imul(h2 ^ ch, 1597334677);
+	}
+	h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+	h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+	h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+	h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
 
-  for (let i = 0; i < len; i++) {
-    hash = ((hash << 5) - hash) + str.charCodeAt(i);
-    hash &= hash; // Convert to 32bit integer
-  }
-
-  return hash;
+	return 4294967296 * (2097151 & h2) + (h1 >>> 0);
 };
 
 /**
